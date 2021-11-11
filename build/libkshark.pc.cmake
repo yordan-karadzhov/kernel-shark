085aa11e9744b5be33835d81cc117c434aa84b38
Yordan Karadzhov (VMware) <y.karadz@gmail.com>
2021-11-11 16:27:21 +0200

    kernel-shark: Add 'Requires' to libkshark.pc
    Adding only the requirements for building 'libkshark'. The requirements
    for building the GUI are not supposed to be listed here.
    
    Signed-off-by: Yordan Karadzhov (VMware) <y.karadz@gmail.com>

--- build/libkshark.pc.cmake
+++ build/libkshark.pc.cmake
@@ -6,5 +6,6 @@ Name: libkshark
 URL: https://git.kernel.org/pub/scm/utils/trace-cmd/kernel-shark.git/
 Description: Library for accessing ftrace file system
 Version: @KS_VERSION_STRING@
+Requires: json-c, libtracecmd >= @LIBTRACECMD_MIN_VERSION@
 Cflags: -I${includedir}
 Libs: -L${libdir} -lkshark
